4e23f157e0ced153cc7f723255773212decc529d
msdanmalan <msdanmallan@gmail.com>
2023-03-10 05:58:29 +0100

    Populate the display when buttons are clicked
    also store display value to a variable for running operations

--- script.js
+++ script.js
@@ -14,24 +14,112 @@ const divide = function(a, b) {
     return a / b
 };
 
-function operate([]) {
-    let numbers = []
-    let res
-    if (numbers.find(number) === '+' )
-    {
-        res = numbers.add(numbers[0], numbers[2])
-    } 
-    else if (numbers.find(number) === '-')
-    {
-        res = numbers.subtract(numbers[0], numbers[2])
+function operate(operator, numA, numB) {
+    let res = 0
+    if (operator === '+') {
+        res = add(numA, numB)
     }
-    else if (numbers.find(number) === '*')
-    {
-        res = numbers.multiply(numbers[0], numbers[2])
+    else if (operator === '-') {
+        res = subtract(numA, numB)
     }
-    else if (numbers.find(number) === '/')
-    {
-        res = numbers.divide(numbers[0], numbers[2])
+    else if (operator === '*') {
+        res = multiply(numA, numB)
     }
-    return res;
+    else if (operator === '/') {
+        res = divide(numA, numB)
+    }
+    return res
 };
+let value = ''
+const display = document.getElementById('display')
+const clear = document.getElementById('clear')
+const plusMinus = document.getElementById('plus/minus')
+
+const point = document.getElementById('point')
+point.addEventListener('click', () => {
+    display.textContent += '.'
+
+})
+
+const modButton = document.getElementById('modButton')
+modButton.addEventListener('click', () => {
+    display.textContent += '%'
+    value += '%'
+})
+const divButton = document.getElementById('divButton')
+divButton.addEventListener('click', () => {
+    display.textContent += '/'
+    value += ` / `
+})
+const multiplyButton = document.getElementById('multiplyButton')
+multiplyButton.addEventListener('click', () => {
+    display.textContent += 'x'
+    value += ` * `
+})
+const plus = document.getElementById('plus')
+plus.addEventListener('click', () => {
+    display.textContent += '+' 
+    value += ` + `
+})
+const minus = document.getElementById('minus')
+minus.addEventListener('click', () => {
+    display.textContent += '-'
+    value += ` - `
+})
+
+const zero = document.getElementById('zero')
+zero.addEventListener('click', () => {
+    display.textContent += '0'
+    value += '0'
+})
+const one = document.getElementById('one')
+one.addEventListener('click', () => {
+    display.textContent += '1'
+    value += '1'
+})
+const two = document.getElementById('two')
+two.addEventListener('click', () => {
+    display.textContent += '2'
+    value += '2'
+})
+const three = document.getElementById('three')
+three.addEventListener('click', () => {
+    display.textContent += '3'
+    value += '3'
+})
+const four = document.getElementById('four')
+four.addEventListener('click', () => {
+    display.textContent += '4'
+    value += '4'
+})
+const five = document.getElementById('five')
+five.addEventListener('click', () => {
+    display.textContent += '5'
+    value += '5'
+})
+const six = document.getElementById('six')
+six.addEventListener('click', () => {
+    display.textContent += '6'
+    value += '6'
+})
+const seven = document.getElementById('seven')
+seven.addEventListener('click', () => {
+    display.textContent += '7'
+    value += '7'
+})
+const eight = document.getElementById('eight')
+eight.addEventListener('click', () => {
+    display.textContent += '8'
+    value += '8'
+})
+const nine = document.getElementById('nine')
+nine.addEventListener('click', () => {
+    display.textContent += '9'
+    value += '9'
+})
+
+const equals = document.getElementById('equals')
+equals.addEventListener('click', () => {
+    display.textContent += '='
+    value.split(' ')
+})
