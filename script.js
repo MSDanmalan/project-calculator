d0be4514fe247240e1fee553c5087203936af59c
msdanmalan <msdanmallan@gmail.com>
2023-03-27 19:46:14 +0100

    Make all buttons functional

--- script.js
+++ script.js
@@ -1,3 +1,131 @@
+// script.js
+
+let value = '' // Use to store variables to operate
+const upperDisplay = document.getElementById('upperDisplay')
+upperDisplay.textContent = ''
+const lowerDisplay = document.getElementById('lowerDisplay')
+lowerDisplay.textContent = ''
+
+const clear = document.getElementById('clearButton')
+clear.addEventListener('click', () => {
+    allClear()
+})
+
+const plusMinus = document.getElementById('plusMinus')
+plusMinus.addEventListener('click', () => {
+    inputPlusMinus()
+    display()
+})
+const point = document.getElementById('point')
+point.addEventListener('click', () => {
+    inputPoint()
+})
+
+const modButton = document.getElementById('modButton')
+modButton.addEventListener('click', () => {
+    value = '%'
+    inputParameters()
+})
+
+const divButton = document.getElementById('divButton')
+divButton.addEventListener('click', () => {
+    value = '/'
+    inputParameters()
+    display()
+})
+
+const multiplyButton = document.getElementById('multiplyButton')
+multiplyButton.addEventListener('click', () => {
+    value = '*'
+    inputParameters()
+    display()
+})
+
+const plus = document.getElementById('plus')
+plus.addEventListener('click', () => {
+    value = '+'
+    inputParameters()
+    display()
+})
+
+const minus = document.getElementById('minus')
+minus.addEventListener('click', () => {
+    value = '-'
+    inputParameters()
+    display()
+})
+
+const zero = document.getElementById('zero')
+zero.addEventListener('click', () => {
+    value = '0'
+    inputParameters()
+    display()
+})
+
+const one = document.getElementById('one')
+one.addEventListener('click', () => {
+    value = '1'
+    inputParameters()
+    display()
+})
+
+const two = document.getElementById('two')
+two.addEventListener('click', () => {
+    value = '2'
+    inputParameters()
+    display()
+})
+
+const three = document.getElementById('three')
+three.addEventListener('click', () => {
+    value = '3'
+    inputParameters()
+    display()
+})
+
+const four = document.getElementById('four')
+four.addEventListener('click', () => {
+    value = '4'
+    inputParameters()
+    display()
+})
+
+const five = document.getElementById('five')
+five.addEventListener('click', () => {
+    value = '5'
+    inputParameters()
+    display()
+})
+
+const six = document.getElementById('six')
+six.addEventListener('click', () => {
+    value = '6'
+    inputParameters()
+    display()
+})
+
+const seven = document.getElementById('seven')
+seven.addEventListener('click', () => {
+    upperDisplay.textContent += '7'
+    value = '7'
+    inputParameters()
+    display()
+})
+
+const eight = document.getElementById('eight')
+eight.addEventListener('click', () => {
+    value = '8'
+    inputParameters()
+    display()
+})
+
+const nine = document.getElementById('nine')
+nine.addEventListener('click', () => {
+    value = '9'
+    inputParameters()
+    display()
+})
+
 const add = function(a, b) {
     return +a + +b
 };
@@ -26,113 +154,164 @@ function operate(operator, numA, numB) {
         res = multiply(numA, numB)
     }
     else if (operator === '/') {
-        res = divide(numA, numB)
+        if(numB === 0){
+            res = 'Error!'
+        }
+        else{
+            res = divide(numA, numB)
+        }
     }
     return res
 };
-let value = ''
-const display = document.getElementById('display')
-display.textContent = ''
-const clear = document.getElementById('clear')
-clear.addEventListener('click', () => {
-    display.textContent = ''
-    value = ''})
-const plusMinus = document.getElementById('plus/minus')
-
-const point = document.getElementById('point')
-point.addEventListener('click', () => {
-    display.textContent += '.'
-})
-
-const modButton = document.getElementById('modButton')
-modButton.addEventListener('click', () => {
-    display.textContent += '%'
-    value += '%'
-})
-const divButton = document.getElementById('divButton')
-divButton.addEventListener('click', () => {
-    display.textContent += '/'
-    value += ` / `
-})
-const multiplyButton = document.getElementById('multiplyButton')
-multiplyButton.addEventListener('click', () => {
-    display.textContent += 'x'
-    value += ` * `
-})
-const plus = document.getElementById('plus')
-plus.addEventListener('click', () => {
-    display.textContent += '+' 
-    value += ` + `
-})
-const minus = document.getElementById('minus')
-minus.addEventListener('click', () => {
-    display.textContent += '-'
-    value += ` - `
-})
-
-const zero = document.getElementById('zero')
-zero.addEventListener('click', () => {
-    display.textContent += '0'
-    value += '0'
-})
-const one = document.getElementById('one')
-one.addEventListener('click', () => {
-    display.textContent += '1'
-    value += '1'
-})
-const two = document.getElementById('two')
-two.addEventListener('click', () => {
-    display.textContent += '2'
-    value += '2'
-})
-const three = document.getElementById('three')
-three.addEventListener('click', () => {
-    display.textContent += '3'
-    value += '3'
-})
-const four = document.getElementById('four')
-four.addEventListener('click', () => {
-    display.textContent += '4'
-    value += '4'
-})
-const five = document.getElementById('five')
-five.addEventListener('click', () => {
-    display.textContent += '5'
-    value += '5'
-})
-const six = document.getElementById('six')
-six.addEventListener('click', () => {
-    display.textContent += '6'
-    value += '6'
-})
-const seven = document.getElementById('seven')
-seven.addEventListener('click', () => {
-    display.textContent += '7'
-    value += '7'
-})
-const eight = document.getElementById('eight')
-eight.addEventListener('click', () => {
-    display.textContent += '8'
-    value += '8'
-})
-const nine = document.getElementById('nine')
-nine.addEventListener('click', () => {
-    display.textContent += '9'
-    value += '9'
-})
 
 const equals = document.getElementById('equals')
 equals.addEventListener('click', () => {
-    valueArray = value.split(' ')
-    x = operate(valueArray[1], valueArray[0], valueArray[2])
-    valueArray.splice(0, 3)
-    for (i = 0; i < valueArray.length; i++) {
-        let y, z
-        y = valueArray[0]
-        z = valueArray[1]
-        a = operate(y, x, z)
-        x = a
-        valueArray.splice(0, 2)
+    if(operandA !== '' && operator !== '' && operandB === ''){
+        allClear()
+        upperDisplay.textContent = 'malformed expression'
     }
-    display.textContent = x
+    else if (operandB === ''){
+        allClear()
+    }
+    else{
+        lowerDisplay.textContent = ''
+        upperDisplay.textContent = result
+        content1 = result
+        operatorSwitch = false
+    }
 })
+
+let operandA = ''
+let operandB = ''
+let operator = ''
+let result = ''
+
+function inputParameters() {
+    if(operator === '' && value !== '+' && value !== '-' && value !== '*' && value !== '/' && value !== '%'){
+        operandA += value
+    }
+    else if(value === '+' || value === '-'|| value === '*'|| value === '/'){
+        if(operandB !== ''){
+            operator = value
+            operandA = result
+            operandB = ''
+            PercentLimitB = true
+        }
+        else{
+            operator = value
+            operandB = ''
+        }
+    }
+    else if(value === '%'){
+        inputPercentage()
+    }
+    else if(operandA !== '' && value !== '+' && value !== '-' && value !== '*' && value !== '/'){
+        operandB += value
+        getResult()
+    }
+}
+
+function getResult() {
+    result = operate(operator, Number(operandA), Number(operandB))
+    lowerDisplay.textContent = result
+    return result
+}
+
+function inputPoint() {
+    if(operandA !== '' && !operandA.includes('.')) {
+        upperDisplay.textContent += '.'
+        operandA += '.';
+    } 
+    else if(operandB !== '' && !operandB.includes('.')){
+        upperDisplay.textContent += '.'
+        operandB += '.'
+    }
+}
+
+// Regulate percentage use to once
+let PercentLimitA = true
+let PercentLimitB = true
+function inputPercentage(){
+    if(operandA !== '' && operandB === '' && PercentLimitA === true) {
+        result = divide(Number(operandA), 100)
+        lowerDisplay.textContent = result
+        operandA = result
+        PercentLimitA = false
+        display()
+    } 
+    else if(operandB !== '' && PercentLimitB === true){
+        result = divide(Number(operandB), 100)
+        lowerDisplay.textContent = result
+        operandB = result
+        getResult()
+        PercentLimitB = false
+        display()
+    }
+    return result
+}
+
+function inputPlusMinus() {
+    if(operandA !== '' && operandB === ''){
+        negative = (Number(operandA) * -1)
+        operandA = negative
+    }
+    else if(operandB !== ''){
+        negative = (Number(operandB) * -1)
+        operandB = negative
+        getResult()
+    }
+}
+let content1 = '' //display content
+let content2 = '' //display content
+let operatorSwitch = false // interchance betwwen operators
+let displayOperatorOnce = true
+let displayPercentOnce = true
+function display(){
+    if(operator === '' && value !== '+' && value !== '-' && value !== '*' && value !== '/' && value !== '%'){
+        upperDisplay.textContent = operandA
+        content1 = upperDisplay.textContent
+        content2 = upperDisplay.textContent
+    }
+    else if(value === '+' || value === '-'|| value === '*'|| value === '/'){
+        if(operatorSwitch === true){
+            extract = content1.substring(0, content1.length - 1)
+            content1 = extract
+            upperDisplay.textContent = content1 + operator
+            content1 = upperDisplay.textContent
+            content2 = upperDisplay.textContent
+        }
+        else if(operatorSwitch === false)
+        {
+            upperDisplay.textContent = content1 + operator
+            content1 = upperDisplay.textContent
+            content2 = upperDisplay.textContent
+            operatorSwitch = true
+        }
+    }
+    else if(value === '%'){
+            upperDisplay.textContent = content1 + value
+            content1 = upperDisplay.textContent
+            content2 = upperDisplay.textContent
+    }
+    else if(operandA !== '' && value !== '+' && value !== '-' && value !== '*' && value !== '/'){
+        upperDisplay.textContent = content2 + operandB
+        content1 = upperDisplay.textContent
+        displayOperatorOnce = true
+    }
+}
+
+// starts afresh
+function allClear(){
+    upperDisplay.textContent = ''
+    lowerDisplay.textContent = ''
+    value = ''
+    operandA = ''
+    operandB = ''
+    operator = ''
+    result = ''
+    PercentLimitA = true
+    PercentLimitB = true
+    content1 = ''
+    operatorSwitch = false
+    displayPercentOnce = true
+}
